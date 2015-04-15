15ed4784407e92232d0e65adbd5fbba5fa9379a1
Omar Rahman <omarr@andrew.cmu.edu>
2015-04-15 14:28:37 -0400

    Initial Commit

---
+++ Geography/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Geography
+//
+//  Created by Omar Rahman on 4/15/15.
+//  Copyright (c) 2015 Omar Rahman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
